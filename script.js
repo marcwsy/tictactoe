f043512d0545b8df30a0b12616a85508c8d2d8c9
marcwsy <marcwsy@gmail.com>
2022-04-07 17:15:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+const playersFactory = (name, marker) => {
+    return { name, marker };
+};
+
+const gameBoard = (() => {
+    const board = ['','','','','','','','','']
+    const modal = document.getElementById("myModal");
+    const btn = document.getElementById("replayBtn");
+    const text = document.querySelector('.text');
+    const squares = document.querySelectorAll('.cell')
+
+    squares.forEach((button, index) => button.addEventListener('click', () => {
+        board[index] = gameStart.active.marker;
+        button.textContent += gameStart.active.marker;
+        button.style.pointerEvents = 'none';
+        gameStart.cellsLeft -= 1;
+        gameStart.checkWin();
+        if (gameStart.winner == false) {
+            if (gameStart.cellsLeft > 0) {
+                gameStart.swapPlayer();
+                gameStart.displayPlayer();
+            } else if (gameStart.cellsLeft == 0) {
+                (gameStart.winner == false)
+                modal.style.display = "block";
+                text.textContent += `it's a tie!`;                
+            }
+        } else{
+            (gameStart.winner == true) 
+                modal.style.display = "block";
+                text.textContent += `${gameStart.active.name} wins`;
+        } 
+    }));
+    
+    btn.addEventListener('click', (e) => {
+        modal.style.display = "none";
+        gameStart.cellsLeft = 9;
+        gameStart.active = gameStart.playerOne;
+        gameStart.winner = false;
+        text.textContent = '';
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+        squares.forEach((button) => {
+            button.textContent = '';
+            button.style.pointerEvents = '';
+        });
+    });
+
+    return {
+        board,
+    }
+})();
+
+
+const gameStart = (() => {
+    const playerOne = playersFactory('player one', 'X');
+    const playerTwo = playersFactory('player two', 'O');
+    const display = document.querySelector('.display');
+
+    let active = playerOne;
+    function swapPlayer() {
+        this.active === playerOne ? this.active = playerTwo : this.active = playerOne;
+    };
+
+    function displayPlayer() {
+        this.active === playerOne ? display.textContent = `${playerOne.name}, go!` : display.textContent = `${playerTwo.name}, go!`;
+    };
+
+    let cellsLeft = 9;
+    let winner = false
+    let winCombo = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    function checkWin() {
+        winCombo.forEach((item, index) => {
+            if (gameBoard.board[item[0]] === this.active.marker && gameBoard.board[item[1]] === this.active.marker && gameBoard.board[item[2]] === this.active.marker) {
+                this.winner = true
+            }
+        });
+    };
+
+    return {
+        active,
+        swapPlayer,
+        displayPlayer,
+        playerOne,
+        playerTwo,
+        cellsLeft,
+        winner,
+        checkWin,    
+    }
+})();
